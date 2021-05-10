cab9b7df1e9ec23a9a18f69248b305453041a646
ismailegilmez <38562312+ismailegilmez@users.noreply.github.com>
2021-05-10 22:39:27 +0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,12 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
-        <span className="icon -love">
+        /**A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        /**<span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="/">Stackrole.com</Link>*/
+        Ismail Egilmez
       </p>
     </div>
   </footer>
